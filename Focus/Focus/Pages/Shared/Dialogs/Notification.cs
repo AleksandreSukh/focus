3b3b2f802e5ff77e6042c69391871352ed921ce4
Aleksandre <aleksandresukhitashvili@gmail.com>
2024-12-10 16:10:40 +0400

    Add notification dialog for displaying errors

---
+++ Focus/Focus/Pages/Shared/Dialogs/Notification.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Systems.Sanity.Focus.Pages.Shared.Dialogs;
+
+internal class Notification
+{
+    private readonly string _message;
+    public Notification(string message)
+    {
+        _message = message;
+    }
+
+    public void Show()
+    {
+        var messageBuilder = new StringBuilder();
+
+        messageBuilder.AppendLine();
+        messageBuilder.AppendLineCentered($"*** {_message} ***");
+        messageBuilder.AppendLine();
+        messageBuilder.AppendLineCentered($"Press \"Enter\" to continue");
+        messageBuilder.AppendLine();
+    }
+}
