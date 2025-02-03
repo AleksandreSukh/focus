f6169b9428e953a2510e075280a70f95d4dff1ed
Aleksandre <aleksandresukhitashvili@gmail.com>
2025-02-03 16:16:10 +0400

    Added build and publish script (repository url hardcoded (also in the code - needs to be moved)

---
+++ Focus/Focus/Build and publish.ps1
@@ -0,0 +1,9 @@
+param([string]$version="1.0.0") 
+#build using .net
+dotnet publish Systems.Sanity.Focus.csproj -c Release --self-contained -r win-x64 -o .\publish
+
+#create package
+vpk pack -u Focus -v $version -p .\publish -e Systems.Sanity.Focus.exe
+
+#publish to GitHub
+vpk upload github --publish --repoUrl https://github.com/AleksandreSukh/focus --token $Env:focusGithubToken
